fdccf97257abbbbf01d3b3b69bc752992efcc9fc
breadbread1984 <breadbread1984@163.com>
2020-03-14 19:05:03 +0800

    update models

--- models.py
+++ models.py
@@ -11,16 +11,15 @@ def CTPN(input_shape, hidden_units = 128, output_units = 512):
   results = tf.keras.layers.Lambda(lambda x: x - tf.reshape([123.68, 116.78, 103.94], (1,1,1,3)))(inputs);
   vgg16 = tf.keras.applications.VGG16(input_tensor = results, include_top = False, weights = 'imagenet');
   # 2) input layer
-  before_reshape = tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3))(vgg16.get_layer('block5_conv3').output);
+  before_reshape = tf.keras.layers.Conv2D(filters = 512, kernel_size = (3,3))(vgg16.get_layer('block5_conv3').output); # before_reshape.shape = (batch, h, w, c = 512)
   # 3) bidirection LSTM
   results = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, x.shape[-2], x.shape[-1])))(before_reshape); # results.shape = (batch * h, w, c = 512)
   results = tf.keras.layers.Bidirectional(layer = tf.keras.layers.LSTM(hidden_units, return_sequences = True), 
                                          backward_layer = tf.keras.layers.LSTM(hidden_units, return_sequences = True, go_backwards = True), 
                                          input_shape = (results.shape[-2], hidden_units),
-                                         merge_mode = 'concat')(results);                             # results.shape = (batch * h, w, c = hidden_units * 2)
-  results = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, x.shape[-1])))(results);              # results.shape = (batch * h * w, c = hidden_units * 2)
-  results = tf.keras.layers.Dense(units = output_units)(results);                                     # results.shape = (batch * h * w, c = output_units)
-  results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, x[1].shape[-3], x[1].shape[-2], x[0].shape[-1])))([results, before_reshape]); # results.shape = (batch, h, w, c = output_units)
+                                         merge_mode = 'concat')(results);                             # results.shape = (batch * h, w, c = 128 * 2)
+  results = tf.keras.layers.Dense(units = output_units)(results);                                     # results.shape = (batch * h, w, c = 512)
+  results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, x[1].shape[-3], x[1].shape[-2], x[0].shape[-1])))([results, before_reshape]); # results.shape = (batch, h, w, c = 512)
   # 4) output layer
   bbox_pred = tf.keras.layers.Dense(units = 10 * 4)(results); # bbox_pred.shape = (batch, h, w, c = 40)
   cls_pred = tf.keras.layers.Dense(units = 10 * 2)(results);  # cls_pred.shape = (batch, h, w, c = 20)
@@ -29,7 +28,7 @@ def CTPN(input_shape, hidden_units = 128, output_units = 512):
   
   return tf.keras.Model(inputs = inputs, outputs = (bbox_pred, cls_pred, cls_prob));
 
-def Loss(img_shape, feat_shape):
+def Loss(img_shape, feat_shape, max_fg_num = 128, max_bg_num = 128, rpn_neg_thres = 0.3, rpn_pos_thres = 0.7):
 
   # constant anchors
   hws = [(11,16),(16,16),(23,16),(33,16),(48,16),(68,16),(97,16),(139,16),(198,16),(283,16)]; # (h,w)
@@ -43,8 +42,8 @@ def Loss(img_shape, feat_shape):
   anchors = np.array(anchors, dtype = np.float32); # anchors.shape = (10, 4)
   
   # graph defined from here
-  bbox_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 40));
-  cls_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 20));
+  bbox_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 40)); # bbox_pred = (1, h, w, 40)
+  cls_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 20)); # cls_pred = (1, h, w, 20)
   gt_bbox = tf.keras.Input((5,)); # gt_bbox.shape = (n, 5) in sequence of (xmin, ymin, xmax, ymax, class)
   # anchor target layer
   grid = tf.keras.layers.Lambda(lambda x: tf.stack([
@@ -67,7 +66,7 @@ def Loss(img_shape, feat_shape):
         tf.math.logical_and(tf.math.greater_equal(x[...,3], 0), tf.math.less(x[...,3], h))
       ),
     )), (-1,))), arguments = {'h': img_shape[-3], 'w': img_shape[-2]})(all_anchors); # anchors = (n, 4) in sequence of (xmin ymin xmax ymax)
-  # bbox overlap
+  # find a anchor box with best iou for every ground truth box
   anchors = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1,1,4)))(anchors); # anchors.shape = (n, 1, 4)
   gt = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (1,-1,5)))(gt_bbox); # gt_bbox.shape = (1, m. 5)
   upperleft = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[0][...,0:2], x[1][...,0:2]))([anchors, gt]); # upperleft.shape = (n,m,2)
@@ -79,8 +78,25 @@ def Loss(img_shape, feat_shape):
   gt_bbox_wh = tf.keras.layers.Lambda(lambda x: x[...,2:4] - x[...,0:2])(gt); # gt_bbox_wh.shape = (1, m, 2)
   gt_bbox_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(gt_bbox_wh); # gt_bbox_area.shape = (1, m)
   iou = tf.keras.layers.Lambda(lambda x: x[0] / (x[1] + x[2] - x[0]))([intersect_area, anchors_area, gt_bbox_area]); # iou.shape = (n, m)
-  best_anchor = tf.keras.layers.Lambda(lambda x: tf.math.argmax(iou, axis = 0, output_type = tf.int32))(iou); # best_anchor.shape = (m)
-  return tf.keras.Model(inputs = (bbox_pred, cls_pred, gt_bbox), outputs = best_anchor);
+  best_anchor = tf.keras.layers.Lambda(lambda x: tf.math.argmax(x, axis = 0, output_type = tf.int32))(iou); # best_anchor.shape = (m,)
+  best_iou = tf.keras.layers.Lambda(lambda x: tf.math.reduce_max(x, axis = 0))(iou); # best_iou.shape = (m)
+  labels = tf.keras.layers.Lambda(lambda x, nt, pt: tf.where(tf.math.less(x, nt), tf.zeros_like(x), tf.where(tf.math.greater(x,pt), tf.ones_like(x), -tf.ones_like(x))), 
+                                  arguments = {'nt': rpn_neg_thres, 'pt': rpn_pos_thres})(best_iou); # labels.shape = (m,)
+  # when there are too many positives, random drop some
+  count = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(tf.where(tf.math.equal(x, 1), tf.ones_like(x), tf.zeros_like(x))))(labels);
+  labels = tf.keras.layers.Lambda(lambda x, n: tf.cond(tf.math.less(x[0], n), 
+                                                       true_fn = lambda: x[1],
+                                                       false_fn = lambda: tf.where(tf.math.logical_and(tf.math.equal(x[1], 1), tf.math.greater(tf.random.uniform(tf.shape(x[1])), n / x[0])), -tf.ones_like(x[1]), x[1])), 
+                                  arguments = {'n': max_fg_num})([count, labels]); # labels.shape = (m,)
+  # when there are too many negatives, random drop some
+  count = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(tf.where(tf.math.equal(x, 0), tf.ones_like(x), tf.zeros_like(x))))(labels);
+  labels = tf.keras.layers.Lambda(lambda x, n: tf.cond(tf.math.less(x[0], n),
+                                                       true_fn = lambda: x[1],
+                                                       false_fn = lambda: tf.where(tf.math.logical_and(tf.math.equal(x[1], 0), tf.math.greater(tf.random.uniform(tf.shape(x[1])), n / x[0])), -tf.ones_like(x[1]), x[1])),
+                                  arguments = {'n': max_bg_num})([count, labels]); # labels.shape = (m,)
+  
+  
+  return tf.keras.Model(inputs = (bbox_pred, cls_pred, gt_bbox), outputs = labels);
 
 if __name__ == "__main__":
 
