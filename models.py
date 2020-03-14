1e7265af5cf8be658b7940abb8ee1abbd0cba1a9
breadbread1984 <breadbread1984@163.com>
2020-03-14 21:18:00 +0800

    update models

--- models.py
+++ models.py
@@ -21,15 +21,12 @@ def CTPN(input_shape, hidden_units = 128, output_units = 512):
   results = tf.keras.layers.Dense(units = output_units)(results);                                     # results.shape = (batch * h, w, c = 512)
   results = tf.keras.layers.Lambda(lambda x: tf.reshape(x[0], (-1, x[1].shape[-3], x[1].shape[-2], x[0].shape[-1])))([results, before_reshape]); # results.shape = (batch, h, w, c = 512)
   # 4) output layer
-  bbox_pred = tf.keras.layers.Dense(units = 10 * 4)(results); # bbox_pred.shape = (batch, h, w, c = 40)
-  bbox_pred = tf.keras.layers.Reshape((bbox_pred.shape[-3], bbox_pred.shape[-2], 10, 4))(bbox_pred); # bbox_pred.shape = (batch, h, w, anchor_num = 10, 4)
-  cls_pred = tf.keras.layers.Dense(units = 10 * 2)(results);  # cls_pred.shape = (batch, h, w, c = 20)
-  cls_pred_reshape = tf.keras.layers.Reshape((cls_pred.shape[-3], cls_pred.shape[-2], 10, 2))(cls_pred); # cls_pred_reshape.shape = (batch, h, w, anchor_num = 10, 2)
-  cls_prob = tf.keras.layers.Softmax()(cls_pred_reshape);
+  bbox_pred = tf.keras.layers.Dense(units = 10 * 6)(results); # bbox_pred.shape = (batch, h, w, c = 60)
+  bbox_pred = tf.keras.layers.Reshape((bbox_pred.shape[-3], bbox_pred.shape[-2], 10, 6))(bbox_pred); # bbox_pred.shape = (batch, h, w, anchor_num = 10, 6) in sequence of (dx dy dw dh logits0 logits1)
   
-  return tf.keras.Model(inputs = inputs, outputs = (bbox_pred, cls_pred, cls_prob));
+  return tf.keras.Model(inputs = inputs, outputs = bbox_pred);
 
-def Loss(img_shape, feat_shape, max_fg_num = 128, max_bg_num = 128, rpn_neg_thres = 0.3, rpn_pos_thres = 0.7):
+def Loss(img_shape, feat_shape, max_fg_anchors = 128, max_bg_anchors = 128, rpn_neg_thres = 0.3, rpn_pos_thres = 0.7):
 
   # constant anchors
   hws = [(11,16),(16,16),(23,16),(33,16),(48,16),(68,16),(97,16),(139,16),(198,16),(283,16)]; # (h,w)
@@ -43,8 +40,7 @@ def Loss(img_shape, feat_shape, max_fg_num = 128, max_bg_num = 128, rpn_neg_thre
   anchors = np.array(anchors, dtype = np.float32); # anchors.shape = (10, 4)
   
   # graph defined from here
-  bbox_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 10, 4)); # bbox_pred = (1, h, w, anchor_num = 10, 4)
-  cls_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 10, 2)); # cls_pred = (1, h, w, anchor_num = 10, 2)
+  bbox_pred = tf.keras.Input((feat_shape[-3], feat_shape[-2], 10, 6)); # bbox_pred = (1, h, w, anchor_num = 10, 4)
   gt_bbox = tf.keras.Input((5,)); # gt_bbox.shape = (n, 5) in sequence of (xmin, ymin, xmax, ymax, class)
   # anchor target layer
   grid = tf.keras.layers.Lambda(lambda x: tf.stack([
@@ -54,59 +50,56 @@ def Loss(img_shape, feat_shape, max_fg_num = 128, max_bg_num = 128, rpn_neg_thre
   all_anchors = tf.keras.layers.Lambda(lambda x, anchors:
     tf.expand_dims(tf.concat([x,x], axis = -1), axis = -2) + tf.reshape(anchors, (1,1,10,4)), # shape = (1, 1, 10, 4)
     arguments = {'anchors': anchors})(grid); # all_anchors.shape = (h, w, 10, 4) in sequence of (xmin ymin xmax ymax)
-  all_anchors = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, 4)))(all_anchors); # all_anchors = (h * w * 10, 4)
-  # filter anchors which crosses over with image borders
-  anchors = tf.keras.layers.Lambda(lambda x, h, w: tf.gather(x, tf.reshape(tf.where(
-    tf.math.logical_and(
-      tf.math.logical_and(
-        tf.math.logical_and(tf.math.greater_equal(x[...,0], 0), tf.math.less(x[...,0], w)),
-        tf.math.logical_and(tf.math.greater_equal(x[...,1], 0), tf.math.less(x[...,1], h))
-      ),
-      tf.math.logical_and(
-        tf.math.logical_and(tf.math.greater_equal(x[...,2], 0), tf.math.less(x[...,2], w)),
-        tf.math.logical_and(tf.math.greater_equal(x[...,3], 0), tf.math.less(x[...,3], h))
-      ),
-    )), (-1,))), arguments = {'h': img_shape[-3], 'w': img_shape[-2]})(all_anchors); # anchors = (n, 4) in sequence of (xmin ymin xmax ymax)
-  # find a anchor box with best iou for every ground truth box
-  anchors = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1,1,4)))(anchors); # anchors.shape = (n, 1, 4)
-  gt = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (1,-1,5)))(gt_bbox); # gt_bbox.shape = (1, m. 5)
-  upperleft = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[0][...,0:2], x[1][...,0:2]))([anchors, gt]); # upperleft.shape = (n,m,2)
-  downright = tf.keras.layers.Lambda(lambda x: tf.math.minimum(x[0][...,2:4], x[1][...,2:4]))([anchors, gt]); # downright.shape = (n,m,2)
-  intersect_wh = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[1] - x[0], 0.))([upperleft, downright]); # intersect_wh.shape = (n,m,2)
-  intersect_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(intersect_wh); # intersect_area.shape = (n,m)
-  anchors_wh = tf.keras.layers.Lambda(lambda x: x[...,2:4] - x[...,0:2])(anchors); # anchors_wh.shape = (n, 1, 2)
-  anchors_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(anchors_wh); # anchors_area.shape = (n, 1)
-  gt_bbox_wh = tf.keras.layers.Lambda(lambda x: x[...,2:4] - x[...,0:2])(gt); # gt_bbox_wh.shape = (1, m, 2)
-  gt_bbox_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(gt_bbox_wh); # gt_bbox_area.shape = (1, m)
-  iou = tf.keras.layers.Lambda(lambda x: x[0] / (x[1] + x[2] - x[0]))([intersect_area, anchors_area, gt_bbox_area]); # iou.shape = (n, m)
-  best_anchor = tf.keras.layers.Lambda(lambda x: tf.math.argmax(x, axis = 0, output_type = tf.int32))(iou); # best_anchor.shape = (m,)
-  best_iou = tf.keras.layers.Lambda(lambda x: tf.math.reduce_max(x, axis = 0))(iou); # best_iou.shape = (m)
+  # get overlaps between anchors and groud truths
+  anchors = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis = -2))(all_anchors); # anchors.shape = (h, w, 10, 1, 4)
+  gt = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (1,1,1,-1,5)))(gt_bbox); # gt.shape = (1, 1, 1, m, 5)
+  upperleft = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[0][...,0:2], x[1][...,0:2]))([anchors, gt]); # upperleft.shape = (h, w, 10, m, 2)
+  downright = tf.keras.layers.Lambda(lambda x: tf.math.minimum(x[0][...,2:4], x[1][...,2:4]))([anchors, gt]); # downright.shape = (h, w, 10, m, 2)
+  intersect_wh = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[1] - x[0], 0.))([upperleft, downright]); # intersect_wh.shape = (h, w, 10, m, 2)
+  intersect_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(intersect_wh); # intersect_area.shape = (h, w, 10, m)
+  anchors_wh = tf.keras.layers.Lambda(lambda x: x[...,2:4] - x[...,0:2] + 1)(anchors); # anchors_wh.shape = (h, w, 10, 1, 2)
+  anchors_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(anchors_wh); # anchors_area.shape = (h, w, 10, 1)
+  gt_bbox_wh = tf.keras.layers.Lambda(lambda x: x[...,2:4] - x[...,0:2] + 1)(gt); # gt_bbox_wh.shape = (1, 1, 1, m, 2)
+  gt_bbox_area = tf.keras.layers.Lambda(lambda x: x[...,0] * x[...,1])(gt_bbox_wh); # gt_bbox_area.shape = (1, 1, 1, m)
+  iou = tf.keras.layers.Lambda(lambda x: x[0] / (x[1] + x[2] - x[0]))([intersect_area, anchors_area, gt_bbox_area]); # iou.shape = (h, w, 10, m)
+  best_iou = tf.keras.layers.Lambda(lambda x: tf.math.reduce_max(x, axis = -1))(iou); # best_iou.shape = (h, w, 10)
+  best_gt_idx = tf.keras.layers.Lambda(lambda x: tf.math.argmax(x, axis = -1, output_type = tf.int32))(iou); # best_gt.shape = (h, w, 10)
+  # 1) label anchor boxes with 1 when overlap > 0.7, 0 when overlap < 0.3, -1 with others
   labels = tf.keras.layers.Lambda(lambda x, nt, pt: tf.where(tf.math.less(x, nt), tf.zeros_like(x), tf.where(tf.math.greater(x,pt), tf.ones_like(x), -tf.ones_like(x))), 
-                                  arguments = {'nt': rpn_neg_thres, 'pt': rpn_pos_thres})(best_iou); # labels.shape = (m,)
+                                  arguments = {'nt': rpn_neg_thres, 'pt': rpn_pos_thres})(best_iou); # labels.shape = (h, w, 10)
   # when there are too many positives, random drop some
   count = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(tf.where(tf.math.equal(x, 1), tf.ones_like(x), tf.zeros_like(x))))(labels);
-  labels = tf.keras.layers.Lambda(lambda x, n: tf.cond(tf.math.less(x[0], n), 
+  labels = tf.keras.layers.Lambda(lambda x, n: tf.cond(tf.math.less(x[0], n),
                                                        true_fn = lambda: x[1],
                                                        false_fn = lambda: tf.where(tf.math.logical_and(tf.math.equal(x[1], 1), tf.math.greater(tf.random.uniform(tf.shape(x[1])), n / x[0])), -tf.ones_like(x[1]), x[1])), 
-                                  arguments = {'n': max_fg_num})([count, labels]); # labels.shape = (m,)
+                                  arguments = {'n': max_fg_anchors})([count, labels]); # labels.shape = (h, w, 10)
   # when there are too many negatives, random drop some
   count = tf.keras.layers.Lambda(lambda x: tf.math.reduce_sum(tf.where(tf.math.equal(x, 0), tf.ones_like(x), tf.zeros_like(x))))(labels);
   labels = tf.keras.layers.Lambda(lambda x, n: tf.cond(tf.math.less(x[0], n),
                                                        true_fn = lambda: x[1],
                                                        false_fn = lambda: tf.where(tf.math.logical_and(tf.math.equal(x[1], 0), tf.math.greater(tf.random.uniform(tf.shape(x[1])), n / x[0])), -tf.ones_like(x[1]), x[1])),
-                                  arguments = {'n': max_bg_num})([count, labels]); # labels.shape = (m,)
-  
-  
-  return tf.keras.Model(inputs = (bbox_pred, cls_pred, gt_bbox), outputs = labels);
+                                  arguments = {'n': max_bg_anchors})([count, labels]); # labels.shape = (h, w, 10)
+  # 2) get prediction
+  anchors_wh = tf.keras.layers.Lambda(lambda x: tf.squeeze(x, axis = -2))(anchors_wh); # anchors_wh.shape = (h, w, 10, 2)
+  anchors_centers = tf.keras.layers.Lambda(lambda x: x[0][..., 0:2] + x[1] / 2)([all_anchors, anchors_wh]); # anchors_centers.shape = (h, w, 10, 2)
+  best_gt = tf.keras.layers.Lambda(lambda x: tf.gather(x[1], x[0]))([best_gt_idx, gt_bbox]); # best_gt.shape = (h, w, 10, 5)
+  best_gt_wh = tf.keras.layers.Lambda(lambda x: x[..., 2:4] - x[..., 0:2] + 1)(best_gt); # best_gt_wh.shape = (h, w, 10, 2)
+  best_gt_centers = tf.keras.layers.Lambda(lambda x: x[0][..., 0:2] + x[1] / 2)([best_gt, best_gt_wh]); # best_gt_centers.shape = (h, w, 10, 2)
+  target_dxdy = tf.keras.layers.Lambda(lambda x: (x[0] - x[1]) / x[2])([best_gt_centers, anchors_centers, anchors_wh]); # target_dxdy.shape = (h, w, 10, 2)
+  target_dwdh = tf.keras.layers.Lambda(lambda x: x[0] / x[1])([best_gt_wh, anchors_wh]); # target_dwdh.shape = (h, w, 10, 2)
+  # TODO
+  bbox_target = tf.keras.layers.Concatenate(axis = -1)([target_dxdy, target_dwdh]); # bbox_target.shape = (h, w, 10, 4)
+    
+  return tf.keras.Model(inputs = (bbox_pred, gt_bbox), outputs = bbox_target);
 
 if __name__ == "__main__":
 
   assert tf.executing_eagerly();
   a = tf.constant(np.random.normal(size = (10,256,256,3)))
   ctpn = CTPN((256,256,3));
-  bbox_pred, cls_pred, cls_prob = ctpn(a)
+  bbox_pred = ctpn(a)
   loss = Loss((256,256,3), bbox_pred.shape);
   b = tf.constant(np.random.normal(size = (10,5)), dtype = tf.float32);
-  anchors = loss([bbox_pred, cls_pred, b]);
+  anchors = loss([bbox_pred, b]);
   print(anchors);
   
