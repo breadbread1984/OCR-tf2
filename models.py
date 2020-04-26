b398c35fb11bfcaa2ba7c4427d4d9df42ad3230b
breadbread1984 <breadbread1984@163.com>
2020-04-26 15:35:29 +0800

    update models

--- models.py
+++ models.py
@@ -140,12 +140,12 @@ def Connector(min_score = 0.7, nms_thres = 0.2, max_horizontal_gap = 50, min_v_o
     return index, bbox, scores;
   _, nms_bbox, nms_bbox_scores = tf.keras.layers.Lambda(lambda x: tf.while_loop(condition, body, loop_vars = [tf.constant(0), x[0], x[1]], shape_invariants = [tf.TensorShape([]), tf.TensorShape([None, 4]), tf.TensorShape([None, 1])]))([sorted_bbox, sorted_bbox_scores]);
   # construct graph
-  minx_diff = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x[...,0], axis = 0) - tf.expand_dims(x[..., 0], axis = 1))(nms_bbox); # successor_mask.shape = (m',m')
+  minx_diff = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x[...,0], axis = 1) - tf.expand_dims(x[..., 0], axis = 0))(nms_bbox); # successor_mask.shape = (m',m')
   # overlap
   upperleft_h = tf.keras.layers.Lambda(lambda x: tf.math.maximum(tf.expand_dims(x[...,1], axis = 0), tf.expand_dims(x[...,1], axis = 1)))(nms_bbox); # upperleft.shape = (m',m')
   downright_h = tf.keras.layers.Lambda(lambda x: tf.math.minimum(tf.expand_dims(x[...,3], axis = 0), tf.expand_dims(x[...,3], axis = 1)))(nms_bbox); # downright.shape = (m',m')
   intersect_h = tf.keras.layers.Lambda(lambda x: tf.math.maximum(x[1] - x[0] + 1, 0.))([upperleft_h, downright_h]); # intersect_wh.shape = (m',m')
-  bbox_h = tf.keras.layers.Lambda(lambda x: nms_bbox[...,3] - nms_bbox[...,1])(nms_bbox); # bbox_h.shape = (m')
+  bbox_h = tf.keras.layers.Lambda(lambda x: x[...,3] - x[...,1])(nms_bbox); # bbox_h.shape = (m')
   max_h = tf.keras.layers.Lambda(lambda x: tf.math.maximum(tf.expand_dims(x, axis = 0), tf.expand_dims(x, axis = 1)))(bbox_h); # max_h.shape = (m',m')
   min_h = tf.keras.layers.Lambda(lambda x: tf.math.minimum(tf.expand_dims(x, axis = 0), tf.expand_dims(x, axis = 1)))(bbox_h); # min_h.shape = (m',m')
   overlap_h = tf.keras.layers.Lambda(lambda x: x[0] / x[1])([intersect_h, min_h]); # overlap.shape = (m',m')
@@ -158,11 +158,19 @@ def Connector(min_score = 0.7, nms_thres = 0.2, max_horizontal_gap = 50, min_v_o
       tf.math.logical_and(tf.math.greater_equal(x[1], t1), tf.math.greater_equal(x[2], t2))
     ),
     arguments = {'g': max_horizontal_gap, 't1': min_v_overlap, 't2': min_size_sim}
-  )([minx_diff,overlap_h,size_similarity]); # is_successor.shape = (m',m')
-  is_precursor = tf.keras.layers.Lambda(lambda x: tf.tranpose(x, (1,0)))(is_successor); # is_precursor.shape = (m',m')
-  
+  )([minx_diff,overlap_h,size_similarity]); # is_successor.shape = (m',m') row is successor of col
+  is_precursor = tf.keras.layers.Lambda(lambda x: tf.transpose(x, (1,0)))(is_successor); # is_precursor.shape = (m',m')
+  row_multiplied_scores = tf.keras.layers.Lambda(lambda x: tf.tile(tf.transpose(x, (1, 0)),(tf.shape(x)[0], 1)))(nms_bbox_scores);
+  def max_by_row(x):
+    mask = x[0];
+    scores = x[1];
+    masked_scores = tf.where(mask, scores, tf.zeros_like(scores));
+    max_index = tf.cond(tf.math.reduce_any(mask), true_fn = lambda: tf.math.argmax(masked_scores), false_fn = lambda: tf.constant(-1, dtype = tf.int64));
+    return max_index;
+  max_precursor_index = tf.keras.layers.Lambda(lambda x: tf.map_fn(max_by_row, (x[0], x[1]), dtype = tf.int64))([is_successor, row_multiplied_scores]);
+  max_successor_index = tf.keras.layers.Lambda(lambda x: tf.map_fn(max_by_row, (x[0], x[1]), dtype = tf.int64))([is_precursor, row_multiplied_scores]);
   # TODO
-  return tf.keras.Model(inputs = (bbox, scores), outputs = minx_diff);
+  return tf.keras.Model(inputs = (bbox, scores), outputs = max_successor_index);
 
 def Loss(max_fg_anchors = 128, max_bg_anchors = 128, rpn_neg_thres = 0.3, rpn_pos_thres = 0.7):
 
