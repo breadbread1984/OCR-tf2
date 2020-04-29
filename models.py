5d79027eafe2b1ca70b38aaf7e9806ad326dcd33
breadbread1984 <breadbread1984@163.com>
2020-04-29 21:06:07 +0800

    update model

--- models.py
+++ models.py
@@ -115,7 +115,7 @@ def GraphBuilder(min_score = 0.7, nms_thres = 0.2, max_horizontal_gap = 50, min_
   def body(index, bbox, scores):
     current_bbox = tf.expand_dims(bbox[index:index + 1,...], axis = 1); # current_bbox.shape = (1, 1, 4)
     following_bbox = tf.expand_dims(bbox[index + 1:,...], axis = 0); # following_bbox.shape = (1, m, 4)
-    following_scores = tf.expand_dims(scores[index:,...], axis = 0); # following_scores.shape = (1, m, 1)
+    following_scores = tf.expand_dims(scores[index + 1:,...], axis = 0); # following_scores.shape = (1, m, 1)
     upperleft = tf.math.maximum(current_bbox[...,:2], following_bbox[...,:2]); # upperleft.shape = (1, m, 2)
     downright = tf.math.minimum(current_bbox[...,2:], following_bbox[...,2:]); # downright.shape = (1, m, 2)
     intersect_wh = tf.math.maximum(downright - upperleft + 1, 0.); # intersect_wh.shape = (1, m, 2)
