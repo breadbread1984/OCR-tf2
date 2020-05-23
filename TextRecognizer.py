39e4e55308f6c07094304669ece2c36ad8d59a7a
breadbread1984 <breadbread1984@163.com>
2020-05-23 22:57:51 +0800

    update text recognizer

--- TextRecognizer.py
+++ TextRecognizer.py
@@ -40,9 +40,9 @@ class TextRecognizer(object):
       inputs = img;
     pred = self.crnn(inputs); # pred.shape = (batch, seq_length, num_class)
     decoded = tf.keras.backend.ctc_decode(pred, [pred.shape[1]], greedy = True);
-    tokens = decoded[0][0]; # the most probabable path.shape = (seq_length, 1)
+    tokens = decoded[0][0]; # the most probabable path.shape = (1, seq_length)
     prob = decoded[1][0]; # the probability of the most probable path.shape = (1,)
-    tokens = tf.squeeze(tokens, axis = 1); # tokens.shape = (seq_length)
+    tokens = tf.squeeze(tokens, axis = 0); # tokens.shape = (seq_length)
     return self.tokenizer.translate(tokens), prob;
 
 if __name__ == "__main__":
